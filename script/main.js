1b4c98c35f44f6d71c7807d07a2da4b58b689572
Hendi Rusfandi <rusfandi.hendi@gmail.com>
2024-11-01 00:00:27 +0700

    finish

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'Adek mau pakai sound ndak?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
